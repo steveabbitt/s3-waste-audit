9c637d98eb36491a9e332b10f6f89df0fc21f02b
Steve Abbitt <steve@mojo-it.com>
2020-12-08 16:16:51 -0800

    initial commit

---
+++ s3-usage-audit.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+BUCKETS=$(aws s3 ls | awk '{ print $3 }' | sort | tr '\n' ' ')
+
+for bucket in $BUCKETS; do
+    echo "Bucket Name: $bucket"
+    echo "Creation Date: $(aws s3 ls | grep $bucket | awk '{ print $1 }')"
+    echo "$(aws s3 ls --summarize --human-readable --recursive $bucket | grep -E -- 'Total Objects|Total Size' | awk '{$1=$1};1')"
+    echo "Newest Object: $(aws s3 ls --recursive $bucket | sort | tail -n 1 | awk '{print $1 " " $4}')"
+    echo
+done

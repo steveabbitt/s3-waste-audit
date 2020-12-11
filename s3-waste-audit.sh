471c27772027deeb5b31c1d2a0abc2a47d4b39b0
Steve Abbitt <steve@mojo-it.com>
2020-12-11 07:44:07 -0800

    adding tail to limit date when buckets have similar names

--- s3-waste-audit.sh
+++ s3-waste-audit.sh
@@ -4,7 +4,7 @@ BUCKETS=$(aws s3 ls | awk '{ print $3 }' | sort | tr '\n' ' ')
 
 for bucket in $BUCKETS; do
     echo "Bucket Name: $bucket"
-    echo "Creation Date: $(aws s3 ls | grep $bucket | awk '{ print $1 }')"
+    echo "Creation Date: $(aws s3 ls | grep $bucket | tail -n 1 | awk '{ print $1 }')"
     echo "$(aws s3 ls --summarize --human-readable --recursive $bucket | grep -E -- 'Total Objects|Total Size' | awk '{$1=$1};1')"
     echo "Newest Object: $(aws s3 ls --recursive $bucket | sort | tail -n 1 | awk '{print $1 " " $4}')"
     echo
